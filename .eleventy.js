038380b1d10498c3327a7d2def7de9bfef67c3d8
vladimir <vlasad@gmail.com>
2022-01-18 13:23:58 +0300

    new site format

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,6 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/favicon.ico");
-  eleventyConfig.addPassthroughCopy("src/favicon-16x16.png");
-  eleventyConfig.addPassthroughCopy("src/favicon-32x32.png");
-
   eleventyConfig.addPassthroughCopy("_redirects");
   eleventyConfig.addPassthroughCopy("_headers");
-
-  eleventyConfig.addPassthroughCopy("src/assets/img/**/*");
 
   return {
     markdownTemplateEngine: "njk",
